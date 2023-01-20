a1b092dd1d5d7c591c0bf4f3f849892749758740
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-20 08:55:49 +0500

    Insert data into the owners table

--- data.sql
+++ data.sql
@@ -29,3 +29,14 @@ VALUES
   ('Boarmon', '2005-06-07', 7, true, 20.40),
   ('Blossom', '1998-10-13', 3, true, 17.00),
   ('Ditto', '2022-05-14', 4, true, 22.00);
+
+--- Insert data into the owners table
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+
