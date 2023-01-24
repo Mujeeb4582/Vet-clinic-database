bc415ba07b54bf45102737845ac8306d34ba2353
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-24 18:34:03 +0500

    Add the schema for invoice table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,18 +1,27 @@
 DROP DATABASE IF EXISTS clinic;
 CREATE DATABASE clinic;
 
--- Add the patient table
+-- Create the patient table
 CREATE TABLE patient (
-  id INTEGER NOT NULL PRIMARY KEY,
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   date_of_birth DATE NOT NULL,
 );
 
--- Add medical histories table
+-- Create medical histories table
 CREATE TABLE medical_histories (
-  id INTEGER NOT NULL PRIMARY KEY,
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   admitted_at TIMESTAMP NOT NULL,
   patient_id INTEGER NOT NULL REFERENCES patient(id) ON DELETE CASCADE,
   status VARCHAR(100),
+);
+
+-- Create the invoice table
+CREATE TABLE invoice (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_amount DECIMAL(10,2) NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP NOT NULL,
+  medical_history_id INTEGER NOT NULL REFERENCES medical_histories(id) ON UPDATE CASCADE,
 );
 
