f158be855c5ee0130a42764bfbd23a8460eabd80
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-24 17:50:30 +0500

    Add the schema for patient table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,2 +1,9 @@
 DROP DATABASE IF EXISTS clinic;
 CREATE DATABASE clinic;
+
+-- Add the patient table
+CREATE TABLE patient (
+  patient_id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  date_of_birth DATE NOT NULL,
+);
