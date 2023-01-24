55217b3fb476535fcb6db1fc600b9995282af9e5
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-24 18:03:02 +0500

    Add the schema for medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,7 +3,16 @@ CREATE DATABASE clinic;
 
 -- Add the patient table
 CREATE TABLE patient (
-  patient_id INTEGER NOT NULL PRIMARY KEY,
+  id INTEGER NOT NULL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   date_of_birth DATE NOT NULL,
 );
+
+-- Add medical histories table
+CREATE TABLE medical_histories (
+  id INTEGER NOT NULL PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INTEGER NOT NULL REFERENCES patient(id) ON DELETE CASCADE,
+  status VARCHAR(100),
+);
+
